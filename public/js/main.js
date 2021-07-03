a167574835ef437c0b9a76d2363672b0fbb3af0d
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-07-03 18:08:20 -0500

    navbar section for small and large devices added. (header pending)

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+//Select the menu (<a> tag)
+const menuButton = document.querySelector('#mobile-menu');
+
+//Change visibility between mobile menu (hidden -> visible)
+menuButton.addEventListener('click', e => {
+	const menu = document.querySelector('.mobile-links');
+	menu.classList.toggle('hidden');
+});
