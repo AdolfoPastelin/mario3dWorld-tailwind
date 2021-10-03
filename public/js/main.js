9a0d832aac39fdeb992e4c80742543b7216d0551
AdolfoPastelin <adolfo.pe97@gmail.com>
2021-10-02 23:51:23 -0500

    minor fixes were made.

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 const menuButton = document.querySelector('#mobile-menu');
 
 //Change visibility between mobile menu (hidden -> visible)
-menuButton.addEventListener('click', e => {
+menuButton.addEventListener('click', () => {
 	const menu = document.querySelector('.mobile-links');
 	menu.classList.toggle('hidden');
 });
